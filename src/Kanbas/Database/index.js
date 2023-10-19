4954beacf2526f6841331aabc1e9adbe6af29b65
ethankong150 <kong.e@northeastern.edu>
2023-10-19 10:04:01 -0400

    a3 done for real

--- src/Kanbas/Database/index.js
+++ src/Kanbas/Database/index.js
@@ -2,8 +2,10 @@ import courses from "./courses.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
 
-export default {
+const database = {
   courses,
   modules,
   assignments,
 };
+
+export default database;
--- src/Labs/a3/BooleanVariables.js
+++ src/Labs/a3/BooleanVariables.js
@@ -9,7 +9,7 @@ function BooleanVariable() {
     let true4 = numberVariable === 123
     let true5 = floatingPointNumber !== 321.432
     let false3 = numberVariable < 100
-    let sortaTrue = '1' == 1  // never use this ==
+    let sortaTrue = '1' === 1  // never use this == //changed because it was causing deployment issues
     let notTrue   = '1' === 1 // always use this ===
     return (
       <div>
